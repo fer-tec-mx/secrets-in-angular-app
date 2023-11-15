3f5b03c49d79cb2a8d208fc9899c729ff581b0c1
Fernando Rendón Bernal <112877708+fer-tec-mx@users.noreply.github.com>
2023-11-15 10:23:01 -0600

    Add file replacements and update dependencies.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { environment } from "./../environments/environment";
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,12 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor() {
+    console.log('environment.production: ', environment.production);
+    console.log('environment.apiUrl: ', environment.apiUrl);
+    console.log('environment.apiKey: ', environment.apiKey);
+  }
+
   title = 'secrets-in-angular-app';
 }
